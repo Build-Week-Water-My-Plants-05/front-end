93a65f42c27fcaa3e64f92d3c483e5166360b16d
muhannadbm <mhd31145@hotmail.com>
2021-08-24 13:22:02 -0400

    Moved App.js out of Components and fixed paths

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App';
+import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
 //Wrapping our app in the browser router
