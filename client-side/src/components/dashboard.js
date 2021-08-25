7624afbbc0f0ace47ac7b65af718be52ee287eeb
Pauline122112 <pauline.design007@gmail.com>
2021-08-24 21:21:33 -0500

    testing to see that route is repsonding

--- client-side/src/components/dashboard.js
+++ client-side/src/components/dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import {React} from 'react'
 import { useState, useEffect } from 'react';
 import axiosWithAuth from './axiosWithAuth/axiosWithAuth';
 import styled from 'styled-components'
@@ -7,52 +7,52 @@ import HomeImage from "./../img/img2.jpg"
 
 
 const Dashboard = (props) => { 
-    const [plants,setPlants] = useState(null)
-    const [watering,setWater] = useState(null)
-    const {trigger,setTrigger} = props
+    // const [plants,setPlants] = useState(null)
+    // const [watering,setWater] = useState(null)
+    // const {trigger,setTrigger} = props
 
-    const history = useHistory()
+    // const history = useHistory()
 
-    useEffect(() => {
-        axiosWithAuth().get('https://wmp-api.herokuapp.com/api/plants').then(res => 
-        setPlants(res.data)
-        )
-    },[watering,trigger])
+    // useEffect(() => {
+    //     axiosWithAuth().get('https://wmp-api.herokuapp.com/api/plants').then(res => 
+    //     setPlants(res.data)
+    //     )
+    // },[watering,trigger])
 
-    const deletePlant = (e,id) => {
+    // const deletePlant = (e,id) => {
 
-        e.preventDefault()
+    //     e.preventDefault()
 
-        axiosWithAuth()
+    //     axiosWithAuth()
 
-            .delete(`/api/plants/${id}`)
-            .then(res => {
+    //         .delete(`/api/plants/${id}`)
+    //         .then( res => {
                
-                console.log('Delete success')
-                setPlants([...plants])
-                history.push('/dashboard')
-                setTrigger(!trigger)
-            })
+    //             console.log('Delete success')
+    //             setPlants([...plants])
+    //             history.push('/dashboard')
+    //             setTrigger(!trigger)
+    //         })
 
-            .catch(err => {
-                console.log(err)
-            })
-    }
+    //         .catch(err => {
+    //             console.log(err)
+    //         })
+    // }
 
-    Date.prototype.addHours= function(h){
-        this.setHours(this.getHours()+h);
-        return this;
-    }
+    // // Date.prototype.addHours= function(h){
+    // //     this.setHours(this.getHours()+h);
+    // //     return this;
+    // // }
 
     
-    const water = (id) => {
-        axiosWithAuth().put(`https://wmp-api.herokuapp.com/api/plants/${id}`,{lastWatered: new Date().toLocaleString()})
-        .then(res =>{
-            var myDate = new Date();
-            console.log(res)
-            setWater(myDate.toString())})
-        .catch( e => console.log(e))
-    }
+    // const water = (id) => {
+    //     axiosWithAuth().put(`https://wmp-api.herokuapp.com/api/plants/${id}`,{lastWatered: new Date().toLocaleString()})
+    //     .then(res =>{
+    //         var myDate = new Date();
+    //         console.log(res)
+    //         setWater(myDate.toString())})
+    //     .catch( e => console.log(e))
+    // }
     
 
     //style component
@@ -65,80 +65,86 @@ const Dashboard = (props) => {
     display: flex;
     justify-content: center;
     align-items: center;
+    opacity: 0.5;
     `
 
 
-    return(
-    <StyledDash>  
-        <Route exact path = '/'></Route> 
+    return (
+			<StyledDash>
+				<Route exact path="/"></Route>
 
-            <h2>My Plants</h2> 
+				<h2>My Plants</h2>
+			</StyledDash>
+		)
 
-        <div className="tablecontainer">
-        {/* <table className="comicGreen"> */}
+        
+
+        // {/* <div className="tablecontainer"> */}
+        // {/* <table className="comicGreen"> */}
           
-            <tr><th>Species</th>
-            <th>Image</th>
-            <th>Name</th>
-            <th>H2o Interval(hrs)</th>
-            <th>H2o Amount</th>
-            <th>Last Watered</th>
-            <th>Date/Time to be watered</th>
-            <th>Water</th><th>Edit</th>
-            <th>Delete</th></tr>
+        //     {/* <tr><th>Species</th>
+        //     <th>Image</th>
+        //     <th>Name</th>
+        //     <th>H2o Interval(hrs)</th>
+        //     <th>H2o Amount</th>
+        //     <th>Last Watered</th>
+        //     <th>Date/Time to be watered</th>
+        //     <th>Water</th><th>Edit</th>
+        //     <th>Delete</th></tr>
 
-            {plants && plants.map(el => {
+        //     {plants && plants.map(el => { */}
 
-                const date = new Date (Date.parse(el.lastWatered))
-                const newdate = new Date(date)
-                newdate.addHours(parseInt(el.h2oInterval))
+    //             const date = new Date (Date.parse(el.lastWatered))
+    //             const newdate = new Date(date)
+    //             newdate.addHours(parseInt(el.h2oInterval))
 
-                let valuetostring = null
-                if(el.h2oInterval === "1"){
-                    valuetostring = "Once every hour"
-                }
+    //             let valuetostring = null
+    //             if(el.h2oInterval === "1"){
+    //                 valuetostring = "Once every hour"
+    //             }
 
-                else if (el.h2oInterval === "2"){
-                    valuetostring = "Once every 2 hours"
-                }
+    //             else if (el.h2oInterval === "2"){
+    //                 valuetostring = "Once every 2 hours"
+    //             }
 
-                else if (el.h2oInterval === "3"){
-                    valuetostring = "Once every 3 hours"
-                }
+    //             else if (el.h2oInterval === "3"){
+    //                 valuetostring = "Once every 3 hours"
+    //             }
 
-                else if (el.h2oInterval === "4"){
-                    valuetostring = "Once every 4 hours"
-                }
+    //             else if (el.h2oInterval === "4"){
+    //                 valuetostring = "Once every 4 hours"
+    //             }
                 
-                const current = new Date()
-                let color = 'good'
+    //             const current = new Date()
+    //             let color = 'good'
 
-                if(current.getTime() > newdate.getTime()) {
-                    color = 'danger'
+    //             if(current.getTime() > newdate.getTime()) {
+    //                 color = 'danger'
 
-                }
+    //             }
                 
-                return<tr><td>{ el.speciesID }</td><td>
-                    <img src={process.env.PUBLIC_URL + `/${el.speciesID}.jpg`} alt="logo" />
-                    </td><td>{el.nickname}</td>
-                    <td>{valuetostring}</td>
-                    <td>{el.h2oAmount}</td>
-                    <td>{el.lastWatered}</td>
+    //             return<tr><td>{ el.speciesID }</td><td>
+    //                 <img src={process.env.PUBLIC_URL + `/${el.speciesID}.jpg`} alt="logo" />
+    //                 </td><td>{el.nickname}</td>
+    //                 <td>{valuetostring}</td>
+    //                 <td>{el.h2oAmount}</td>
+    //                 <td>{el.lastWatered}</td>
 
-                <td className={color}>
-                    {newdate.toLocaleString()}</td>
+    //             <td className={color}>
+    //                 {newdate.toLocaleString()}</td>
 
-                <td><button className="water" onClick={() => water(el.plantID)}>
-                    </button></td><td><Link to={`/edit/${el.plantID}`} className="edit">Edit</Link></td>
-                    <td><button className="delete" onClick = {(e) => deletePlant(e,el.plantID)}>X</button></td></tr>
+    //             <td><button className="water" onClick={() => water(el.plantID)}>
+    //                 </button></td><td><Link to={`/edit/${el.plantID}`} className="edit">Edit</Link></td>
+    //                 <td><button className="delete" onClick = {(e) => deletePlant(e,el.plantID)}>X</button></td></tr>
 
-            })}
+    //         })}
             
 
            
-    </div>
-    </StyledDash>
-)
+    
+    // </div>
+    
+    
 }
 
 
