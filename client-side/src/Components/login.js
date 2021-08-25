37d8b7f4e2cf82b46e7cff02a7a8442154913370
stevenjhomem <sjhomem@outlook.com>
2021-08-25 11:51:52 -0500

    commiting to get help from Casey with a small issue

--- client-side/src/Components/login.js
+++ client-side/src/Components/login.js
@@ -1,7 +1,11 @@
+import React, { useEffect, useState } from 'react';
 import {Route} from 'react-router-dom';
 import styled from 'styled-components';
 import UserAvatar from './img/img1.jpg';
-
+import * as yup from 'yup';
+import YupPassword from 'yup-password';
+import { isSchema } from 'yup';
+YupPassword(yup);
 
 
 const StyledDiv = styled.div`
@@ -49,7 +53,7 @@ const StyledForm = styled.form`
     flex-direction: column;
     justify-content: space-around;
     align-items: center;
-    height: 150px;
+    height: 200px;
     font-weight: bolder;
     width: 300px;
 `;
@@ -61,8 +65,50 @@ const StyledEntryBoxes = styled.div`
     height: 100px;
 `
 
+const initialFormValues = {
+    username: '',
+    password: '',
+}
+
+const loginSchema = yup.object().shape({
+    username : yup.string().required().min('4', 'Username must be at least 4 characters in length'),
+    password : yup.string().required().matches(
+        /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+        "Password must contain at least 8 characters one uppercase, one number and one special case character"
+      ),
+})
+
 
 const Login = () => {
+    //Set up or slices of state//
+    const [ formValues, setFormValues ] = useState(initialFormValues);
+    const [ errors, setErrors ] = useState(initialFormValues);
+    const [ disabledUsername, setDisabledUsername ] = useState(false);
+    // const [ disabledPassword, setDisabledPassword ] = useState(true);
+
+    //Function that validates form entries according to the schema which can be found above//
+    const setFormErrors = (name, value) => {
+        yup.reach(loginSchema, name).validate(value)
+        .then(() => setErrors({...errors,[name]:''}))
+        .catch(err => setErrors({...errors, [name]:err.errors[0]}))
+    }
+
+    //Function that handles form changes and updates the form and errors//
+    const change = event => {
+        const {value,name} = event.target;
+        setFormErrors(name,value);
+        setFormValues({...formValues, [name]:value});
+    }
+
+    useEffect(()=>{
+        loginSchema.isValid(formValues.username)
+        .then(valid => {
+            setDisabledUsername(!valid)
+            console.log(valid)
+    })
+    }, [formValues]);
+
+
 
     return (
     <StyledDiv>
@@ -79,10 +125,27 @@ const Login = () => {
                     <StyledForm>
                         <StyledEntryBoxes>
                         <label> Username: 
-                            <input type ='text' placeholder = ''></input>
+                            <input 
+                                name = 'username'
+                                value = {formValues.username}
+                                type = 'text' 
+                                placeholder = ''
+                                onChange ={change}></input>
                         </label>
+                        {disabledUsername && <div style = {
+                            {marginTop: '5px',
+                            marginBottom: '5px',
+                            color: 'red',
+                            backgroundColor: 'pink',
+                            width: '75.2%',
+                            border: '1px solid red'}}>{errors.username}</div>}
                         <label>Password: 
-                        <input type = 'text' placeholder = 'Case Sensitive'></input>
+                            <input 
+                                name = 'password'
+                                value = {formValues.password}
+                                type = 'text' 
+                                placeholder = 'Case Sensitive'
+                                onChange = {change} ></input>
                         </label>
                         </StyledEntryBoxes>
                         <button type = 'submit'>Login!</button>
