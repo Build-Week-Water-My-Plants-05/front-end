55499eabcee97c764d1e28d057177955fda0bb29
Pauline122112 <pauline.design007@gmail.com>
2021-08-24 20:33:40 -0500

    navigating through some errors

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import AddPlant from './AddPlant';
 import SignUp from './SignUp';
 import PlantEdit from './PlantEdit';
-
+import PrivateRoute from './PrivateRoute'
 import { Route, Link, Switch } from "react-router-dom";
 import Home from './Home.js';
 import Login from './login';
 import styled from 'styled-components';
+import Dashboard from './components/dashboard';
 
 
 const StyledHeader = styled.div`
@@ -35,37 +36,32 @@ function App() {
 
 
   return (
-    <div>
-   
-      <StyledHeader>
+		<div>
+			<StyledHeader>
+				<StyledLink to="/">
+					<h1>Watery-Minder</h1>
+				</StyledLink>
 
-          <StyledLink to = '/'><h1>Watery-Minder</h1></StyledLink> 
+				<StyledAccountButtons>
+					<StyledLink to="/usersignup">
+						<h5>Sign Up</h5>
+					</StyledLink>
 
-          <StyledAccountButtons>
+					<StyledLink to="/userlogin">
+						<h5>Login</h5>
+					</StyledLink>
+				</StyledAccountButtons>
+			</StyledHeader>
 
-            <StyledLink to = '/usersignup'>
-             <h5>Sign Up</h5> 
-            </StyledLink> 
-
-            <StyledLink to = '/userlogin' >
-            <h5>Login</h5>
-            </StyledLink>
-
-          </StyledAccountButtons>
-
-      </StyledHeader>
-
-      
-      <Switch>
-        <Route exact path = '/' component = {Home}/>
-        <Route path = '/usersignup' component = {SignUp}/> 
-        <Route path = '/userlogin' component = {Login}/>
-        <Route path = '/editplant/:id' component = {PlantEdit}/>
-      </Switch>
-
-     
-    </div>
-  );
+			<Switch>
+				<PrivateRoute exact path="/dashboard" component={Dashboard} />
+				<Route exact path="/" component={Home} />
+				<Route path="/usersignup" component={SignUp} />
+				<Route path="/userlogin" component={Login} />
+				<Route path="/editplant/:id" component={PlantEdit} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
---
+++ client-side/src/components/dashboard.js
@@ -0,0 +1,109 @@
+
+import React from 'react'
+import { useState, useEffect } from 'react';
+import axiosWithAuth from './axiosWithAuth/axiosWithAuth';
+import styled from 'styled-components'
+
+
+import {
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useLocation,
+    useHistory
+  } from "react-router-dom";
+
+const Dashboard = (props) => { 
+    const [plants,setPlants] = useState(null)
+    const [watering,setWater] = useState(null)
+    const {trigger,setTrigger} = props
+    const history = useHistory()
+
+    useEffect(() => {
+        axiosWithAuth().get('https://wmp-api.herokuapp.com/api/plants').then(res => 
+        setPlants(res.data)
+        )
+    },[watering,trigger])
+
+    const deletePlant = (e,id) => {
+
+        e.preventDefault()
+
+        axiosWithAuth()
+
+            .delete(`/api/plants/${id}`)
+            .then(res => {
+               
+                console.log('Delete success')
+                setPlants([...plants])
+                history.push('/dashboard')
+                setTrigger(!trigger)
+            })
+
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    Date.prototype.addHours= function(h){
+        this.setHours(this.getHours()+h);
+        return this;
+    }
+
+    
+    const water = (id)=> {
+        axiosWithAuth().put(`https://wmp-api.herokuapp.com/api/plants/${id}`,{lastWatered: new Date().toLocaleString()})
+        .then(res =>{
+            var myDate = new Date();
+            console.log(res)
+            setWater(myDate.toString())})
+        .catch(e=> console.log(e))
+    }
+    
+    return(
+    <div>   
+            <h2>My Plants</h2> 
+        <div className="tablecontainer">
+        {/* <table className="comicGreen"> */}
+          
+            <tr><th>Species</th><th>Image</th><th>Nick name</th><th>H2o Interval(hours)</th><th>H2o Amount</th><th>Last Watered</th><th>Date/Time to be watered</th><th>Water</th><th>Edit</th><th>Delete</th></tr>
+            {plants && plants.map(el => {
+                const date = new Date(Date.parse(el.lastWatered));
+                const newdate = new Date(date)
+                newdate.addHours(parseInt(el.h2oInterval))
+                let valuetostring = null
+                if(el.h2oInterval === "1"){
+                    valuetostring = "Once every hour"
+                }
+                else if (el.h2oInterval === "2"){
+                    valuetostring = "Once every 2 hours"
+                }
+                else if (el.h2oInterval === "3"){
+                    valuetostring = "Once every 3 hours"
+                }
+                else if (el.h2oInterval === "4"){
+                    valuetostring = "Once every 4 hours"
+                }
+                
+                const current = new Date()
+                let color = 'good'
+                if(current.getTime() > newdate.getTime()) {
+                    color = 'danger'
+                }
+                
+                return  <tr><td>{el.speciesID}</td><td><img src={process.env.PUBLIC_URL + `/${el.speciesID}.jpg`} alt="logo" /></td><td>{el.nickname}</td><td>{valuetostring}</td><td>{el.h2oAmount}</td><td>{el.lastWatered}</td>
+                <td className={color}>{newdate.toLocaleString()}</td>
+                <td><button className="water" onClick={()=>water(el.plantID)}></button></td><td><Link to={`/edit/${el.plantID}`} className="edit">Edit</Link> </td><td><button className="delete" onClick={(e)=> deletePlant(e,el.plantID)}>X</button></td></tr>
+
+            })}
+            
+
+           
+    </div>
+    </div>
+)
+}
+
+
+export default Dashboard
