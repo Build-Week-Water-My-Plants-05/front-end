d08ca86c90e88556273d5a5e1a6922e33bab4f8e
Pauline122112 <pauline.design007@gmail.com>
2021-08-25 12:48:02 -0500

    something is wrong with Date.prototype.addHours and I have no idea why

--- client-side/src/components/dashboard.js
+++ client-side/src/components/dashboard.js
@@ -6,179 +6,169 @@ import { Link, useHistory, Route } from "react-router-dom";
 import HomeImage from "./../img/img2.jpg"
 
 
-const Dashboard = (props) => { 
-    // const [plants,setPlants] = useState(null)
-    // const [watering,setWater] = useState(null)
-    // const {trigger,setTrigger} = props
+const Dashboard = (props) => {
+	const [plants,setPlants] = useState(null)
+	const [watering,setWater] = useState(null)
+	const {trigger,setTrigger} = props
 
-    // const history = useHistory()
+	const history = useHistory()
 
-    // useEffect(() => {
-    //     axiosWithAuth().get('https://wmp-api.herokuapp.com/api/plants').then(res => 
-    //     setPlants(res.data)
-    //     )
-    // },[watering,trigger])
+	useEffect(() => {
+	    axiosWithAuth().get('https://wmp-api.herokuapp.com/api/plants').then(res =>
+	    setPlants(res.data)
+	    )
+	},[watering,trigger])
 
-    // const deletePlant = (e,id) => {
+	const deletePlant = (e,id) => {
+		e.preventDefault()
+		axiosWithAuth()
+			.delete(`/api/plants/${id}`)
+			.then((res) => {
+				console.log("Delete success");
+				setPlants([...plants]);
+				history.push("/dashboard");
+				setTrigger(!trigger);
+			})
 
-    //     e.preventDefault()
+			.catch((err) => {
+				console.log("DELETE ERR", err)
+			});
+	}
 
-    //     axiosWithAuth()
+	Date.prototype.addHours= function(h){
+	    this.setHours(this.getHours()+h);
+	    return this;
+	}
 
-    //         .delete(`/api/plants/${id}`)
-    //         .then( res => {
-               
-    //             console.log('Delete success')
-    //             setPlants([...plants])
-    //             history.push('/dashboard')
-    //             setTrigger(!trigger)
-    //         })
+	const water = (id) => {
+	    axiosWithAuth().put(`https://wmp-api.herokuapp.com/api/plants/${id}`,
+	    {lastWatered: new Date().toLocaleString()})
+	    .then(res => {
+	        var myDate = new Date();
+	        console.log(res)
+	        setWater(myDate.toString())})
 
-    //         .catch(err => {
-    //             console.log(err)
-    //         })
-    // }
+	    .catch( e => console.log(e))
+	}
 
-    // Date.prototype.addHours= function(h){
-    //     this.setHours(this.getHours()+h);
-    //     return this;
-    // }
+	//style component
+	const StyledDash = styled.div`
+		background-image: url("${HomeImage}");
+		background-size: cover;
+		background-position: center;
+		height: 80vh;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		opacity: 0.5;
+	`;
 
-    
-    // const water = (id) => {
+	const StyledTable = styled.table`
+		margin: auto;
+		width: 50%;
+		font-family: "Comic Sans MS", cursive, sans-serif;
+		border: 2px solid #4f7849;
+		background-color: #eeeeee;
+		width: 72%;
+		text-align: center;
+		border-collapse: collapse;
+		td,
+		th {
+			border: 1px solid #4f7849;
+			padding: 3px 2px;
+		}
 
-    //     axiosWithAuth().put(``,
-        
-    //     {lastWatered: new Date().toLocaleString()})
-
-    //     .then( res => {
-
-    //         var myDate = new Date();
-    //         console.log(res)
-
-    //         setWater(myDate.toString())})
-
-    //     .catch( e => console.log(e))
-    // }
-    
-
-    //style component
-    const StyledDash = styled.div`
-    background-image: url('${HomeImage}');
-    background-size: cover;
-    background-position: center;
-    height: 80vh;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    opacity: 0.5;
-    `
-
-    const StyledTable = styled.table`
-			margin: auto;
-			width: 50%;
-			font-family: "Comic Sans MS", cursive, sans-serif;
-			border: 2px solid #4f7849;
-			background-color: #eeeeee;
-			width: 72%;
+		tbody td {
+			font-size: 19px;
+			font-weight: bold;
+			color: #4f7849;
+		}
+		tr:nth-child(even) {
+			background: #cee0cc;
+		}
+		thead {
+			background: #207808;
+			background: -moz-linear-gradient(
+				top,
+				#589a46 0%,
+				#368520 66%,
+				#207808 100%
+			);
+			background: -webkit-linear-gradient(
+				top,
+				#589a46 0%,
+				#368520 66%,
+				#207808 100%
+			);
+			background: linear-gradient(
+				to bottom,
+				#589a46 0%,
+				#368520 66%,
+				#207808 100%
+			);
+			border-bottom: 1px solid #444444;
+		}
+		thead th {
+			font-size: 15px;
+			font-weight: bold;
+			color: #ffffff;
 			text-align: center;
-			border-collapse: collapse;
-			td,
-			th {
-				border: 1px solid #4f7849;
-				padding: 3px 2px;
-			}
-			tbody td {
-				font-size: 19px;
-				font-weight: bold;
-				color: #4f7849;
-			}
-			tr:nth-child(even) {
-				background: #cee0cc;
-			}
-			thead {
-				background: #207808;
-				background: -moz-linear-gradient(
-					top,
-					#589a46 0%,
-					#368520 66%,
-					#207808 100%
-				);
-				background: -webkit-linear-gradient(
-					top,
-					#589a46 0%,
-					#368520 66%,
-					#207808 100%
-				);
-				background: linear-gradient(
-					to bottom,
-					#589a46 0%,
-					#368520 66%,
-					#207808 100%
-				);
-				border-bottom: 1px solid #444444;
-			}
-			thead th {
-				font-size: 15px;
-				font-weight: bold;
-				color: #ffffff;
-				text-align: center;
-				border-left: 0px solid #d0e4f5;
-			}
-			thead th:first-child {
-				border-left: none;
-			}
-			tfoot td {
-				font-size: 21px;
-			}
-			.water {
-				padding: 1rem;
-				background: #1887dc;
-				color: white;
-				font-weight: bold;
-				cursor: pointer;
-				width: 25px;
-				height: 25px;
-				border-radius: 0 83% 53% 80%;
-				border: 3px solid black;
-				transform: rotate(45deg);
-				margin-top: 12px;
-			}
-			.water:hover {
-				background-color: #53b4ff;
-			}
-			.edit:hover {
-				background-color: #60c260;
-			}
-			.edit {
-				background: #4eb74e;
-				border-radius: 50%;
-				padding: 0.6rem;
-				text-decoration: none;
-				color: white;
-			}
-			.delete:hover {
-				background-color: #ffa6a6b0;
-			}
-			.delete {
-				color: red;
-				background: #ff9999b0;
-				font-weight: bold;
-				border-radius: 50%;
-				font-size: 1rem;
-				padding: 0.6rem;
-				border: 0;
-				cursor: pointer;
-			}
-		`; 
+			border-left: 0px solid #d0e4f5;
+		}
+		thead th:first-child {
+			border-left: none;
+		}
+		tfoot td {
+			font-size: 21px;
+		}
+		.water {
+			padding: 1rem;
+			background: #1887dc;
+			color: white;
+			font-weight: bold;
+			cursor: pointer;
+			width: 25px;
+			height: 25px;
+			border-radius: 0 83% 53% 80%;
+			border: 3px solid black;
+			transform: rotate(45deg);
+			margin-top: 12px;
+		}
+		.water:hover {
+			background-color: #53b4ff;
+		}
+		.edit:hover {
+			background-color: #60c260;
+		}
+		.edit {
+			background: #4eb74e;
+			border-radius: 50%;
+			padding: 0.6rem;
+			text-decoration: none;
+			color: white;
+		}
+		.delete:hover {
+			background-color: #ffa6a6b0;
+		}
+		.delete {
+			color: red;
+			background: #ff9999b0;
+			font-weight: bold;
+			border-radius: 50%;
+			font-size: 1rem;
+			padding: 0.6rem;
+			border: 0;
+			cursor: pointer;
+		}
+	`;
 
+	return (
+		<StyledDash>
+			<Route exact path="/"></Route>
 
-    return (
-			<StyledDash>
-				<Route exact path="/"></Route>
-
-				<h2>My Plants</h2>
+			<div>
+				<h2>Dashboard</h2>
 
 				<div className="tablecontainer">
 					{/* <table className="comicGreen"> */}
@@ -197,7 +187,7 @@ const Dashboard = (props) => {
 							<th>Delete</th>
 						</tr>
 
-						{/* {plants &&
+						 {plants &&
 							plants.map((el) => {
 								const date = new Date(Date.parse(el.lastWatered));
 								const newdate = new Date(date);
@@ -217,7 +207,7 @@ const Dashboard = (props) => {
 								const current = new Date();
 								let color = "good";
 
-								if (current.getTime() > newdate.getTime()) {
+								if (current.getTime()>newdate.getTime()) {
 									color = "danger";
 								}
 
@@ -258,11 +248,12 @@ const Dashboard = (props) => {
 										</td>
 									</tr>
 								);
-							})} */}
+							})} 
 					</StyledTable>
 				</div>
-			</StyledDash>
-		);
+			</div>
+		</StyledDash>
+	);
 }
 
 
